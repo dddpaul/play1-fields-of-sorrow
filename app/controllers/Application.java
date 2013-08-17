84ff5f2ef8c16d34ce333c36e7150af61204e79d
Pavel Derendyaev <paul@reic.ru>
2013-08-17 13:35:28 +0300

    Initial commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.*;
+
+public class Application extends Controller
+{
+    public static void index()
+    {
+        render();
+    }
+
+    public static void add()
+    {
+        params.flash();
+        index();
+    }
+}
